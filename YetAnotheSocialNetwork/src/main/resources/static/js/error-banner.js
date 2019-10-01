d536dbf353a933b55d8f851f804e8d0b1f690798
DenislavVelichkov <denislav.velichkov@gmail.com>
2019-10-01 18:28:54 +0300

    Front-End updated, Bad credentials message on Log In fixed.

--- YetAnotheSocialNetwork/src/main/resources/static/js/error-banner.js
+++ YetAnotheSocialNetwork/src/main/resources/static/js/error-banner.js
@@ -1,22 +1,26 @@
 (function () {
-    let alert_element = $('<div class="alert alert-danger alert-dismissable">\n' +
-        '<a class="close" data-dismiss="alert" aria-label="close">×</a>\n' +
-        '<strong>Wrong email address or Password!</strong> Please enter your credentials again.\n' +
-        '</div>');
+    let alert_element =
+        $('<div class="bad-credentials-label-container">\n' +
+            '<div id="bad-credentials" class="alert alert-danger alert-dismissable">\n' +
+            '<a class="close" data-dismiss="alert" aria-label="close">×</a>\n' +
+            '<strong>Wrong email address or Password!</strong> Please enter your credentials again.\n' +
+            '</div>\n' +
+            '</div>'
+        )
+    ;
 
     alert_element.css('margin', 'auto');
     alert_element.css('padding', 'auto');
     alert_element.css('font-size', '1rem');
     alert_element.css('display', 'inline-block');
     alert_element.css('text-align', 'center');
-    alert_element.css('vertical-align', 'bottom');
     alert_element.css('width', '100%');
 
     setTimeout(() => {
         alert_element.appendTo(document.querySelector('#login-form-container'));
 
         setTimeout(() => {
-            $('div.alert').remove();
+            $('div#bad-credentials').remove();
         }, 5000)
     }, 100);
 })();
