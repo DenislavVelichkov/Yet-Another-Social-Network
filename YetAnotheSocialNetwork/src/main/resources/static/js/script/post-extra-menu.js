1583190287df512c707c6eaad1b45ae87e0430bc
DenislavVelichkov <denislav.velichkov@gmail.com>
2019-11-12 15:58:38 +0200

    Home Page- Wall Added. Post Comment new feature added.

--- YetAnotheSocialNetwork/src/main/resources/static/js/script/post-extra-menu.js
+++ YetAnotheSocialNetwork/src/main/resources/static/js/script/post-extra-menu.js
@@ -1,29 +1,29 @@
-let postPicInPost = function () {
-    $('.yasn-post-extra-photo').show();
+let postPrivacyToggle = function () {
+    $('.yasn-post-extra-privacy ul').toggle();
+    $('.yasn-post-extra-photo').hide();
     $('.yasn-post-extra-location').hide();
     $('.yasn-post-extra-tag').hide();
-    $('.yasn-post-extra-privacy').hide()
+};
+
+let postPicInPost = function () {
+    $('.yasn-post-extra-photo').toggle();
+    $('.yasn-post-extra-location').hide();
+    $('.yasn-post-extra-tag').hide();
+    $('.yasn-post-extra-privacy ul').hide();
 };
 
 let postLocationInPost = function () {
-    $('.yasn-post-extra-location').show();
+    $('.yasn-post-extra-location').toggle();
     $('.yasn-post-extra-photo').hide();
+    $('.yasn-post-extra-privacy ul').hide();
     $('.yasn-post-extra-tag').hide();
-    $('.yasn-post-extra-privacy').hide()
 };
 
 let postTagFriends = function () {
-    $('.yasn-post-extra-tag').show();
+    $('.yasn-post-extra-tag').toggle();
     $('.yasn-post-extra-location').hide();
     $('.yasn-post-extra-photo').hide();
-    $('.yasn-post-extra-privacy').hide()
-};
-
-let postPrivacyToggle = function () {
-    $('.yasn-post-extra-privacy').show();
-    $('.yasn-post-extra-tag').hide();
-    $('.yasn-post-extra-location').hide();
-    $('.yasn-post-extra-photo').hide();
+    $('.yasn-post-extra-privacy ul').hide();
 };
 
 $(document).ready(function () {
