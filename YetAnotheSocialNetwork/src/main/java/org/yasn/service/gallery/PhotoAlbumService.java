fa5014c3fbb0eed7fbba08792e432606529d4f80
DenislavVelichkov <denislav.velichkov@gmail.com>
2019-12-28 13:11:22 +0200

    Code Refactoring

--- YetAnotheSocialNetwork/src/main/java/org/yasn/service/gallery/PhotoAlbumService.java
+++
@@ -1,7 +0,0 @@
-package org.yasn.service.gallery;
-
-import org.yasn.data.models.service.gallery.PhotoAlbumServiceModel;
-
-public interface PhotoAlbumService {
-  PhotoAlbumServiceModel getAlbumById(String id);
-}
