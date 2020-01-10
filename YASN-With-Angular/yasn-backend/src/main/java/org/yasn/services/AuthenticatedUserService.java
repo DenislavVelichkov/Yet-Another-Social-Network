a1816f67171dd6a02cd419f50af374b7957055f5
DenislavVelichkov <denislav.velichkov@gmail.com>
2020-01-10 14:13:02 +0200

    git rebase

--- YASN-With-Angular/yasn-backend/src/main/java/org/yasn/services/AuthenticatedUserService.java
+++
@@ -1,9 +0,0 @@
-package org.yasn.services;
-
-import java.util.List;
-
-public interface AuthenticatedUserService {
-  String getUsername();
-
-  List<String> getRoles();
-}
--- YASN-With-Angular/yasn-backend/src/main/java/org/yasn/services/gallery/PhotoAlbumService.java
+++
@@ -1,7 +0,0 @@
-package org.yasn.services.gallery;
-
-import org.yasn.data.models.service.gallery.PhotoAlbumServiceModel;
-
-public interface PhotoAlbumService {
-  PhotoAlbumServiceModel getAlbumById(String id);
-}
